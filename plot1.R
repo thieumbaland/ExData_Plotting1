4821673f80c019d64cf7a895c7bbb7d61eed7464
Mathieu Wauters <mwauters@deloitte.com>
2016-01-09 00:29:15 +0100

    Amendments

--- plot1.R
+++ plot1.R
@@ -10,11 +10,13 @@ household_data <- read.csv(file="data/household_power_consumption.txt",sep=";",h
 household_df<-tbl_df(household_data)
 household_df$Date<-dmy(household_df$Date)
 household_df$Time<-hms(household_df$Time)
-household_df_trunc<-dplyr::filter(household_df,Date>"2007-01-31" & Date<"2007-02-03")
+date_lb<-"2007-02-01"
+date_ub<-"2007-02-03"
+household_df_trunc<-household_df[household_df$Date<date_ub & household_df$Date>=date_lb,]
 
 cols = c(3:ncol(household_df_trunc));    
 household_df_trunc[,cols] = apply(household_df_trunc[,cols], 2, function(x) as.numeric(as.character(x)))
 #make plot
 png(file="plot1.png",width=480,height=480)
-with(household_df_trunc,hist(Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)"))
+myplot<-with(household_df_trunc,hist(Global_active_power,col="red",cex.axis=0.7,main="Global Active Power",xlab="Global Active Power (kilowatts)"))
 dev.off()
