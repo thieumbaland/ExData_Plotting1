84bc404247677c5caa498d982594f1e015c9ab5a
Mathieu Wauters <mwauters@deloitte.com>
2016-01-09 01:25:14 +0100

    Minor changes

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
-#please see plot1.R to find out how we saved the .RData file that is called here
+source("sourceFiles.R")
 Sys.setlocale("LC_TIME", "English")
-load(file="household_data_trunc.RData")
 household_df_trunc$DateTime<-dmy_hms(household_df_trunc$DateTime,tz="UCT",truncated=3)
 
 png(file="plot2.png",height=480,width=480)
